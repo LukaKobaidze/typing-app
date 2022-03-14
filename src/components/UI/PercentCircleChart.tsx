b72c4359c12e77381224aaea5553e8b149586740
Luka <lukakobaidze2006@gmail.com>
2022-03-14 12:33:05 +0400

    Added <PercentCircleChart /> component

--- src/components/UI/PercentCircleChart.tsx
+++ src/components/UI/PercentCircleChart.tsx
@@ -6,12 +6,20 @@ interface Props {
 }
 
 const PercentCircleChart = ({ percentage, className }: Props) => {
+  const style = { '--percent': percentage } as React.CSSProperties;
+
   return (
-    <div className={`percent-circle-chart ${className}`}>
+    <div className={`percent-circle-chart ${className}`} style={style}>
       <div className="percent-circle-chart__empty" />
       <div className="percent-circle-chart__filled" />
       <div className="percent-circle-chart__middle">
-        <p>{percentage}</p>
+        <p
+          className={`percent-circle-chart__text percent-circle-chart__text--${
+            percentage >= 75 ? 'success' : 'fail'
+          }`}
+        >
+          {percentage}%
+        </p>
       </div>
     </div>
   );
