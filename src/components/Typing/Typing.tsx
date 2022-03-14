4ca50aefec637954803466b47df88aba5ecd385c
Luka <lukakobaidze2006@gmail.com>
2022-03-14 12:30:09 +0400

    Added <Typing /> component to type and test your typing speed

--- src/components/Typing/Typing.tsx
+++ src/components/Typing/Typing.tsx
@@ -1,46 +1,62 @@
-import { TypingState } from 'shared/types/typing.type';
+import { useContext, useEffect } from 'react';
+import TypingContext from 'context/typing-context';
 import { Timer } from 'components/UI';
 import Word from './Word';
 import Result from './Result';
 import Reset from './Reset';
 import 'styles/Typing/Typing.scss';
 
-interface Props {
-  typingState: TypingState;
-  type: (key: string) => void;
-  reset: () => void;
-  deleteKey: () => void;
-}
+const Typing = () => {
+  const { state, dispatch } = useContext(TypingContext);
 
-const Typing = ({ typingState, type, reset, deleteKey }: Props) => {
-  const typeHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === 'Backspace') {
-      return deleteKey();
+  useEffect(() => {
+    let interval: NodeJS.Timer;
+
+    if (state.typingStarted) {
+      interval = setInterval(() => {
+        dispatch({ type: 'TIME_DECREMENT' });
+      }, 1000);
+    } else {
+      clearInterval(interval!);
     }
 
-    if (event.key.length === 1) {
-      return type(event.key);
-    }
-  };
+    return () => clearInterval(interval);
+  }, [state.typingStarted, dispatch]);
+
+  useEffect(() => {
+    const typeHandler = (event: KeyboardEvent) => {
+      if (event.key === 'Backspace') {
+        return dispatch({ type: 'DELETE_KEY' });
+      }
+
+      if (event.key.length === 1) {
+        return dispatch({ type: 'TYPE', payload: event.key });
+      }
+    };
+
+    document.addEventListener('keydown', typeHandler);
+
+    return () => document.removeEventListener('keydown', typeHandler);
+  }, [dispatch]);
 
   return (
-    <div className="typing" tabIndex={-1} onKeyDown={typeHandler}>
-      <Timer seconds={typingState.timerCountdown} />
+    <div className="typing">
+      <Timer seconds={state.timerCountdown} />
       <div className="typing__words">
-        {typingState.words.map((word, index) => (
+        {state.words.map((word, index) => (
           <Word
             key={index}
             word={word}
-            isCurrentWord={index === typingState.currentWord}
-            currentLetter={typingState.currentLetter}
+            isCurrentWord={index === state.currentWord}
+            currentLetter={state.currentLetter}
           />
         ))}
       </div>
-      <Reset reset={reset} />
-      {typingState.timerCountdown === 0 && (
+      <Reset />
+      {state.timerCountdown === 0 && (
         <Result
-          typedWords={typingState.words.slice(0, typingState.currentWord + 1)}
-          secondsTook={typingState.initialTime}
+          typedWords={state.words.slice(0, state.currentWord + 1)}
+          secondsTook={state.initialTime}
         />
       )}
     </div>
