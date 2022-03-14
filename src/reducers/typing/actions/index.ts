469f98c8eb6278c0240b8abb14c8838c0c45d034
Luka <lukakobaidze2006@gmail.com>
2022-03-14 13:45:01 +0400

    commit

--- src/reducers/typing/actions/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as reset } from './reset.action';
-export { default as type } from './type.action';
-export { default as deleteKey } from './deleteKey.action';
-export { default as countdownDecrement } from './countdownDecrement.action';
-export { default as setTimer } from './setTimer.action';
-export { default as setDifficulty } from './setDifficulty.action';
--- src/reducers/typing/functions/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as transformWordsArray } from './transformWordsArray.function';
-export { default as getRandomWords } from './getRandomWords.function';
