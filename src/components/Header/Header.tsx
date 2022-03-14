f51c13489c14dce723353b942d5bd48e25fe6fcd
Luka <lukakobaidze2006@gmail.com>
2022-03-14 12:21:40 +0400

    removed props that were passed to <Settings />

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,12 @@
 import { Logo } from 'components/UI';
-import { TypingDifficulty, TypingTime } from 'shared/types/typing.type';
+import Settings from './Settings';
 import 'styles/Header/Header.scss';
-import QuickSettings from './QuickSettings';
 
-interface Props {
-  difficulty: TypingDifficulty;
-  timer: TypingTime;
-  changeDifficulty: (arg0: TypingDifficulty) => void;
-  changeTimer: (arg0: TypingTime) => void;
-}
-
-const Header = (props: Props) => {
-  const { difficulty, timer, changeDifficulty, changeTimer } = props;
-
+const Header = () => {
   return (
     <header className="header">
       <Logo />
-      <QuickSettings
-        difficulty={difficulty}
-        timer={timer}
-        changeDifficulty={changeDifficulty}
-        changeTimer={changeTimer}
-      />
+      <Settings />
     </header>
   );
 };
