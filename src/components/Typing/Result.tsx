6160037628cfe493fac45acc5e483867207b7253
Luka <lukakobaidze2006@gmail.com>
2022-03-14 12:28:51 +0400

    Added <Results /> component to show results (WPM and Accuracy) after typing

--- src/components/Typing/Result.tsx
+++ src/components/Typing/Result.tsx
@@ -1,5 +1,5 @@
+import { TypingWordsType } from 'context/state-types';
 import { PercentCircleChart } from 'components/UI';
-import { TypingWordsType } from 'shared/types/typing.type';
 import 'styles/Typing/Result.scss';
 
 const getWordsData = (words: TypingWordsType) => {
@@ -49,32 +49,22 @@ interface Props {
 }
 
 const Result = ({ typedWords, secondsTook }: Props) => {
-  const { totalWords, wordsCorrect, totalLetters, lettersCorrect } =
+  const { wordsCorrect, totalLetters, lettersCorrect } =
     getWordsData(typedWords);
 
-  // Calculate words per minute
-  const calcWPM = () => {
-    return wordsCorrect / (secondsTook / 60);
-  };
-
-  // Calculate percentage of correct words
-  const calcAccuracy = () => {
-    return +((lettersCorrect / totalLetters) * 100).toFixed(2);
-  };
-
-  console.log(`${calcWPM()}WPM`);
-  console.log(`Accuracy ${calcAccuracy()}%`);
+  const calcWPM = wordsCorrect / (secondsTook / 60);
+  const calcAccuracy = +((lettersCorrect / totalLetters) * 100).toFixed(2);
 
   return (
     <div className="result">
       <div className="result__wpm">
         <p>Words per minute (WPM)</p>
-        <p className="result__number">{calcWPM()}</p>
+        <p className="result__number">{calcWPM}</p>
       </div>
       <div className="result__accuracy">
         <p>Accuracy</p>
         <PercentCircleChart
-          percentage={calcAccuracy()}
+          percentage={calcAccuracy}
           className="result__number"
         />
       </div>
