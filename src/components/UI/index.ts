06b7410588e8a27ff63a14e7b61773b943994c25
Luka <lukakobaidze2006@gmail.com>
2022-03-14 12:31:30 +0400

    Added index.ts file to import components

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,5 @@
 export { default as Logo } from './Logo';
 export { default as PercentCircleChart } from './PercentCircleChart';
 export { default as TextButton } from './TextButton';
-export {default as TextOnHover} from './TextOnHover';
+export { default as TextOnHover } from './TextOnHover';
 export { default as Timer } from './Timer';
