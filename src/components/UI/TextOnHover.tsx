ac966e1ee497eaf17ab804662c2dff7f0b541bf4
Luka <lukakobaidze2006@gmail.com>
2022-03-16 11:22:26 +0400

    now this component takes position prop

--- src/components/UI/TextOnHover.tsx
+++ src/components/UI/TextOnHover.tsx
@@ -2,18 +2,27 @@ import 'styles/UI/TextOnHover.scss';
 
 interface Props {
   text: string;
+  position?: 'top' | 'right' | 'bottom' | 'left';
   children: React.ReactNode;
   classNameWrapper?: string;
   className?: string;
 }
 
 const TextOnHover = (props: Props) => {
-  const { text, children, classNameWrapper, className } = props;
+  const {
+    text,
+    position = 'bottom',
+    children,
+    classNameWrapper,
+    className,
+  } = props;
 
   return (
     <div className={`text-on-hover ${classNameWrapper}`}>
       {children}
-      <div className={`text-on-hover__text-div ${className}`}>
+      <div
+        className={`text-on-hover__text-div text-on-hover__text-div--${position} ${className}`}
+      >
         <p>{text}</p>
       </div>
     </div>
