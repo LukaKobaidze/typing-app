68ec03edba2982aaf5d40ad87ba6fe69ad67cd34
Luka <lukakobaidze2006@gmail.com>
2022-03-25 17:44:11 +0400

    Changed folder name from 'functions' to 'utils'

---
+++ src/context/utils/getRandomWords.ts
@@ -0,0 +1,24 @@
+import randomWords from 'random-words';
+import { TypingDifficulty, TypingWordsType } from 'shared/types';
+
+const getRandomWords = (
+  difficulty: TypingDifficulty = 'medium',
+  quantity: number = 100
+): TypingWordsType => {
+  let words: string[];
+
+  switch (difficulty) {
+    case 'medium':
+      words = randomWords({ exactly: quantity, maxLength: 4 });
+      break;
+    case 'hard':
+      words = randomWords(quantity);
+      break;
+  }
+
+  return words.map((word: string) =>
+    word.split('').map((letter) => ({ letter, type: 'none', extra: false }))
+  );
+};
+
+export default getRandomWords;
