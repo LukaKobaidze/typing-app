57b5e1c42c8b58b373e7556cb4aad8ba0ad0bbf5
Luka <lukakobaidze2006@gmail.com>
2022-04-08 13:44:43 +0400

    added chart - timeline of wpm and accuracy. and changed layout

--- src/components/Typing/TypingResult.tsx
+++ src/components/Typing/TypingResult.tsx
@@ -1,74 +1,110 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { TypingContext } from 'context';
-import { TypingWordsType } from 'shared/types';
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Label,
+  Tooltip,
+  Legend,
+  Line,
+} from 'recharts';
 import { PercentCircleChart } from 'components/UI';
+import TypingRestart from './TypingRestart';
 import styles from 'styles/Typing/TypingResult.module.scss';
 
-const getWordsData = (words: TypingWordsType) => {
-  let totalLetters: number = 0;
-  let lettersCorrect: number = 0;
-
-  words.forEach((word) => {
-    let isWordCorrect = true;
-    word.forEach((letter) => {
-      const isLetterCorrect = letter.type === 'correct';
-      if (letter.type !== 'none') {
-        totalLetters++;
-      }
-      if (isWordCorrect) {
-        isWordCorrect = isLetterCorrect;
-      }
-      if (isLetterCorrect) {
-        lettersCorrect++;
-      }
-    });
-    if (isWordCorrect) {
-      // Including spaces
-      totalLetters++;
-      lettersCorrect++;
-    }
-  });
-
-  return {
-    totalLetters,
-    lettersCorrect,
-  };
+const config = {
+  colorWpm: '#dcdcdc',
+  colorAccuracy: '#00faa7',
+  labelOffset: -40,
+  labelFontSize: 14,
 };
 
-interface Props {
-  typedWords: TypingWordsType;
-  secondsTook: number;
-}
-
-const TypingResult = ({ typedWords, secondsTook }: Props) => {
-  const { dispatch } = useContext(TypingContext);
-  const { totalLetters, lettersCorrect } = getWordsData(typedWords);
-
-  const calcWPM = lettersCorrect / 5 / (secondsTook / 60);
-  const calcAccuracy = +((lettersCorrect / totalLetters) * 100).toFixed(2);
-
-  useEffect(() => {
-    dispatch({
-      type: 'ADD_RESULT',
-      payload: { wpm: calcWPM, accuracy: calcAccuracy },
-    });
-  }, [calcWPM, calcAccuracy, dispatch]);
+const TypingResult = () => {
+  const {
+    state: { results },
+    dispatch,
+  } = useContext(TypingContext);
 
   return (
     <div className={styles['result__wrapper']}>
       <div className={styles.result}>
-        <div className={styles['result__wpm']}>
-          <p>Words per minute (WPM)</p>
-          <p className={styles['result__number']}>{calcWPM}</p>
+        <div className={styles['wpm-accuracy-container']}>
+          <div className={styles.wpm}>
+            <p>WPM</p>
+            <p className={styles['wpm__num']}>{results.wpm}</p>
+          </div>
+          <div className={styles.accuracy}>
+            <p>Accuracy</p>
+            <PercentCircleChart
+              percentage={results.accuracy}
+              className={styles['percentage-circle']}
+            />
+          </div>
         </div>
-        <div className={styles['result__accuracy']}>
-          <p>Accuracy</p>
-          <PercentCircleChart
-            percentage={calcAccuracy}
-            className={styles['result__percent-circle']}
-          />
+        <div className={styles.chart}>
+          <ResponsiveContainer className={styles.chart}>
+            <LineChart data={results.timeline}>
+              <XAxis dataKey="second" />
+              <YAxis dataKey="wpm" yAxisId="left">
+                <Label
+                  value="wpm"
+                  angle={-90}
+                  fill={config.colorWpm}
+                  fontSize={config.labelFontSize}
+                  position="right"
+                  offset={config.labelOffset}
+                  className={styles.label}
+                />
+              </YAxis>
+              <YAxis
+                domain={[0, 100]}
+                dataKey="accuracy"
+                yAxisId="right"
+                orientation="right"
+              >
+                <Label
+                  value="accuracy"
+                  angle={-90}
+                  fill={config.colorAccuracy}
+                  fontSize={config.labelFontSize}
+                  position="left"
+                  offset={config.labelOffset}
+                  className={styles.label}
+                />
+              </YAxis>
+              <CartesianGrid opacity={0.15} />
+              <Tooltip contentStyle={{ backgroundColor: '#0d0d1d' }} />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="wpm"
+                yAxisId="left"
+                dot={{
+                  stroke: config.colorWpm,
+                  strokeWidth: 2,
+                  r: 2,
+                }}
+                stroke={config.colorWpm}
+              />
+              <Line
+                type="monotone"
+                dataKey="accuracy"
+                yAxisId="right"
+                dot={{
+                  stroke: config.colorAccuracy,
+                  strokeWidth: 2,
+                  r: 2,
+                }}
+                stroke={config.colorAccuracy}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </div>
+      <TypingRestart className={styles.restart} />
     </div>
   );
 };
