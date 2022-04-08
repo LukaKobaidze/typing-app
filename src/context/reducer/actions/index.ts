83bc6f854ba303c36eb2b148cbc210a62c52556b
Luka <lukakobaidze2006@gmail.com>
2022-04-08 13:35:41 +0400

    changed order of imports/exports

--- src/context/reducer/actions/index.ts
+++ src/context/reducer/actions/index.ts
@@ -1,9 +1,9 @@
-export { default as restart } from './restart.action';
 export { default as type } from './type.action';
 export { default as skipWord } from './skipWord.action';
 export { default as deleteKey } from './deleteKey.action';
 export { default as deleteWord } from './deleteWord.action';
-export { default as timeDecrement } from './timeDecrement.action';
-export { default as setTime } from './setTimer.action';
+export { default as restart } from './restart.action';
 export { default as setDifficulty } from './setDifficulty.action';
-export { default as addResult } from './addResult.action';
+export { default as setTime } from './setTime.action';
+export { default as timeDecrement } from './timeDecrement.action';
+export { default as result } from './result.action';
