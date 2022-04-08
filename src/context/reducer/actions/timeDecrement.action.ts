b167259ea4eed53e64e35562d788964c76751506
Luka <lukakobaidze2006@gmail.com>
2022-04-08 13:41:31 +0400

    add words to wordsTimeline

--- src/context/reducer/actions/timeDecrement.action.ts
+++ src/context/reducer/actions/timeDecrement.action.ts
@@ -1,16 +1,14 @@
 import { TypingState } from 'shared/types';
 
 const timeDecrement = (state: TypingState): TypingState => {
-  if (state.timerCountdown === 0) {
-    return {
-      ...state,
-      typingStarted: false,
-    };
-  }
+  if (state.timerCountdown === 0) return state;
+
+  const typedWords = state.words.slice(0, state.wordIndex + 1);
 
   return {
     ...state,
     timerCountdown: state.timerCountdown - 1,
+    wordsTimeline: [...state.wordsTimeline, typedWords],
   };
 };
 
