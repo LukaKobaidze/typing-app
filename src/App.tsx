c2f22ff31906b3e919374c93f34cfcb7bb72bbdf
Luka <lukakobaidze2006@gmail.com>
2022-04-15 12:47:13 +0400

    moved TypingContextProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { TypingContextProvider } from 'context';
 import { Header, Main, Footer } from 'layouts';
 
 const App = () => (
-  <>
+  <TypingContextProvider>
     <Header />
     <Main />
     <Footer />
-  </>
+  </TypingContextProvider>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { TypingContextProvider } from 'context';
 import App from 'App';
 import 'styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TypingContextProvider>
-      <App />
-    </TypingContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
