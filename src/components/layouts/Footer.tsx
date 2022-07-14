67a379e1477bb98bcabcb9a88b6358ae21354ecb
Luka <lukakobaidze2006@gmail.com>
2022-07-14 17:47:11 +0400

    removed Links folder

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,14 +1,26 @@
+import { ReactComponent as IconGithub } from 'assets/images/github.svg';
 import { useContext } from 'react';
 import { GlobalContext } from 'context';
-import Links from '../Links';
 import styles from 'styles/layouts/Footer.module.scss';
+import { TextOnHover } from 'components/UI';
 
 const Footer = () => {
   const { typingStarted } = useContext(GlobalContext);
 
   return (
     <footer className={`${styles.footer} ${typingStarted ? 'hide' : ''}`}>
-      <Links />
+      <div className={styles.links}>
+        <TextOnHover text="repository" position="top">
+          <a
+            href="https://github.com/LukaKobaidze/typing-app"
+            rel="noreferrer"
+            target="_blank"
+            className="links-item__link"
+          >
+            <IconGithub />
+          </a>
+        </TextOnHover>
+      </div>
     </footer>
   );
 };
