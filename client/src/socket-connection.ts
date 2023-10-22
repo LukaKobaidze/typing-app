a17df1705cb8b72e269aa9cfea53540be7019dff
Luka <lukakobaidze.dev@gmail.com>
2023-10-22 15:41:09 +0400

    update

--- client/src/socket-connection.ts
+++ client/src/socket-connection.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://typing-app.fly.dev');
+const socket = io(process.env.API_URL || 'http://localhost:8080');
 
 export default socket;
