14d5b96906a0f26b6a2586eada07a67d6a8b4c72
Luka <lukakobaidze.dev@gmail.com>
2023-10-23 17:14:04 +0400

    Fixed api connection url

--- client/src/socket-connection.ts
+++ client/src/socket-connection.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
-// const socket = io('https://typing-app.fly.dev');
+// const socket = io('http://localhost:8080');
+const socket = io('https://typing-app.fly.dev');
 
 export default socket;
