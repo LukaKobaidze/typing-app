7534b879d6c6a93eb0e78cb366badca8e4fb1d31
Luka <lukakobaidze.dev@gmail.com>
2023-11-05 22:21:04 +0400

    Updated socket connection code

--- client/src/socket-connection.ts
+++ client/src/socket-connection.ts
@@ -2,11 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io(
   process.env.NODE_ENV === 'production'
-    ? process.env.WEBSOCKET_URL ||
-        (() => {
-          console.error("Environment variable `WEBSOCKET_URL` wasn't provided");
-          return 'http://localhost:8080';
-        })()
+    ? 'https://typing-app.fly.dev'
     : 'http://localhost:8080'
 );
 
