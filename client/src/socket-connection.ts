59871dd9e036ca801b589cfc9f249c925f6dc994
Luka <lukakobaidze.dev@gmail.com>
2023-11-06 18:21:43 +0400

    using vite env.PROD for production check

--- client/src/socket-connection.ts
+++ client/src/socket-connection.ts
@@ -1,9 +1,7 @@
 import { io } from 'socket.io-client';
 
 const socket = io(
-  import.meta.env.NODE_ENV === 'production'
-    ? import.meta.env.WEBSOCKET_URL
-    : 'http://localhost:8080'
+  import.meta.env.PROD ? import.meta.env.WEBSOCKET_URL : 'http://localhost:8080'
 );
 
 export default socket;
