4242c2471019245b810fd5e192eb051681928778
Luka <lukakobaidze.dev@gmail.com>
2024-02-14 18:45:25 +0400

    Changed default typemode

--- client/src/context/typemode.context.tsx
+++ client/src/context/typemode.context.tsx
@@ -14,7 +14,7 @@ interface Context {
   onQuoteLength: (length: QuoteLengthType) => void;
 }
 const initial: Context = {
-  mode: 'time',
+  mode: 'quote',
   time: 15,
   wordsAmount: 10,
   quoteLength: 'short',
