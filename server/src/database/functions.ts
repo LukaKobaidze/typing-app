e3c3ae2d432cc04c495e023af97f9cc66fb06adb
LukaKobaidze <lukakobaidze.dev@gmail.com>
2024-03-27 22:09:58 +0400

    Removed unnecessary dependency

--- server/src/database/functions.ts
+++ server/src/database/functions.ts
@@ -1,15 +1,9 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-const MONGODB_CONNECTION = process.env.MONGODB_CONNECTION;
+const MONGODB_CONNECTION = process.env.MONGODB_CONNECTION!;
 
 export async function databaseConnect() {
-  if (!MONGODB_CONNECTION) {
-    throw new Error(
-      "Couldn't connect to the database. `MONGODB_CONNECTION` is undefined"
-    );
-  }
-
   await mongoose.connect(MONGODB_CONNECTION);
 }
 
